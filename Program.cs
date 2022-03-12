6299c40e1f9dce85c8454484e689d5fe31e9b45f
CyberAngel <j.pacuta@gmail.com>
2022-03-12 09:19:42 +0100

    příklad vyříznutí části textu a výpočet Crc32

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ConsoleApp3
 {
@@ -10,6 +10,22 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            // načtení obsahu souboru do stringu
+            string json = File.ReadAllText("text.txt");
+
+            // dohledání klíče "soubory" pomocí JSON parseru
+            var jsonObjekt = JObject.Parse(json);
+            var soubory = jsonObjekt["soubory"].ToString(Formatting.None);
+
+            // odstranění hranatých závorek na začátku/konci a uvozovek na konci
+            var obsah = soubory.TrimStart('[').TrimEnd(new[] { ']', '"'});
+
+            // výpočet CRC32
+            var crc32 = new Crc32();
+
+            var soucet = crc32.Get(obsah.ToArray());
+
+            Console.WriteLine(soucet);
         }
     }
 }
